861ba73ed12e811a4d7a2fd7f7642978ebf06b87
zara <zara030396@gmail.com>
2019-04-01 00:51:35 +0400

    Done iter_swap.cpp

--- mutating_algoritms/iter_swap.cpp
+++ mutating_algoritms/iter_swap.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <algorithm>
 #include <numeric>
 #include <vector>
 
+template <class Assignable>
+
+void swap(Assignable& a, Assignable& b)
+{
+    Assignable c(a);
+    a = b;
+    b = c;
+}
+
 template <class ForwardIt1, class ForwardIt2>
 
-void iter_swap(ForwardIt1 a, ForwardIt2 b)
+void iter_swap(ForwardIt1& a, ForwardIt2& b)
 {
     swap(*a, *b);
 }
@@ -22,15 +30,15 @@ int main()
     }
 
     std::vector<int> v1(12);
+    std::vector<int>::iterator it1;
     iota(v1.begin(), v1.end(), 4);
     std::cout <<"\nMy v1 vector ";
 
     for (it  = v1.begin(); it != v1.end(); ++it) {
 	std::cout << *it << " ";
     }
-
     std::cout <<"\n\nswapping v and v1 \n";
-    iter_swap(v, v1);
+    iter_swap(v.begin(), v1.begin() + 4);
    
     std::cout <<"\nMy v vector ";
 
